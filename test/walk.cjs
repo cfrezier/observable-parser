ea5f6d2dbebe099c841b34e79c2063e3c9da32ea
Christophe Frézier <c.frezier@groupeonepoint.com>
2021-11-22 11:59:23 +0100

    Fixing file base path problem in test on CI

--- test/walk.cjs
+++ test/walk.cjs
@@ -1,22 +1,23 @@
 const walk = require("../lib/walk");
 const options = require("../lib/options-management");
+const basename = require("../lib/basename");
 
 const expect = require('chai').expect;
 
 describe('walk', () => {
     it('should visit everything and call function', (done) => {
         walk('.', options, (err, res) => {
-            expect(res).to.eql(
+            expect(res.map(file => basename(file))).to.eql(
                 [
-                    '/Users/c.frezier/IdeaProjects/observable-parser/test/combine-latest-with-fn.class.ts',
-                    '/Users/c.frezier/IdeaProjects/observable-parser/test/combine-latest.class.ts',
-                    '/Users/c.frezier/IdeaProjects/observable-parser/test/ngrx-store-selectorFn.class.ts',
-                    '/Users/c.frezier/IdeaProjects/observable-parser/test/ngrx-store.class.ts',
-                    '/Users/c.frezier/IdeaProjects/observable-parser/test/no-property-combine.class.ts',
-                    '/Users/c.frezier/IdeaProjects/observable-parser/test/no-property.class.ts',
-                    '/Users/c.frezier/IdeaProjects/observable-parser/test/or-definition.class.ts',
-                    '/Users/c.frezier/IdeaProjects/observable-parser/test/simple-observable.class.ts',
-                    '/Users/c.frezier/IdeaProjects/observable-parser/test/using-injection.class.ts'
+                    '/test/combine-latest-with-fn.class.ts',
+                    '/test/combine-latest.class.ts',
+                    '/test/ngrx-store-selectorFn.class.ts',
+                    '/test/ngrx-store.class.ts',
+                    '/test/no-property-combine.class.ts',
+                    '/test/no-property.class.ts',
+                    '/test/or-definition.class.ts',
+                    '/test/simple-observable.class.ts',
+                    '/test/using-injection.class.ts'
                 ]);
             done();
         })
